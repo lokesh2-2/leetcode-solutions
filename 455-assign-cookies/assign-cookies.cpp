34f0faf9a1d1c4a1c101fe0cf001a4b9f2170493
lokesh2-2 <23211a05d4@bvrit.ac.in>
2026-03-24 11:41:04 +0530

    Time: 6 ms (75.94%) | Memory: 44.8 MB (77.92%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) 
+    {
+        int count=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=g.size()-1,j=s.size()-1;
+        while(i>=0 && j>=0)
+        {
+            if(s[j]>=g[i])
+            {
+                count++;
+                i--;
+                j--;
+            }
+            else i--;
+        }
+        return count;
+    }
+};
